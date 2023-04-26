8639da6d732d6c671f8962904686b9d54ae95418
Ben Gillott <52325353+ben-gillott@users.noreply.github.com>
2023-04-25 22:29:39 -0700

    hamburger on mobile

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,29 +5,29 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
 
   return (
     <div className={topBarClass}>
-      <div className="wrapper">
-        <div className="left">
-          <a href="/" className="name">
-            Ben Gillott
-          </a>
-        </div>
-        <div className="right">
-          <div className="sectionLink">
-            <a href="/">About</a>
-          </div>
+      <div className="left">
+        <a href="/" className="name">
+          Ben Gillott
+        </a>
+      </div>
 
-          <div className="sectionLink">
+      <div className="menu">
+        <ul>
+          <li>
+            <a href="/" onClick={() => setMenuOpen(false)}>
+              About
+            </a>
+          </li>
+
+          <li onClick={() => setMenuOpen(false)}>
             <a href="/portfolio">Portfolio</a>
-          </div>
-
-          {/* <div className="sectionLink">
-            <a href="/blog">Blog</a>
-          </div> */}
-
-          {/* <div className="sectionLink">
-            <a href="/art">Art</a>
-          </div> */}
-        </div>
+          </li>
+        </ul>
+      </div>
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <span className="line1"></span>
+        <span className="line2"></span>
+        <span className="line3"></span>
       </div>
     </div>
   );
