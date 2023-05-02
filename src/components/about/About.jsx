e7234171695b0daccc8e0b87a52189a3d820c0e3
Ben Gillott <52325353+ben-gillott@users.noreply.github.com>
2023-05-01 21:03:02 -0700

    mobile about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,82 +12,86 @@ import instaIcon from "../../assets/icons/instagram.svg";
 export default function About() {
   return (
     <div className="about" id="about">
-      <Loader
-        elem={
-          <div className="imageContainer">
-            <img className="image" src={profileImg} alt="Profile" />
+      <div className="left">
+        <div className="imageContainer">
+          <Loader
+            elem={<img className="image" src={profileImg} alt="Profile" />}
+          />
+        </div>
+
+        <div className="iconContainer">
+          <div className="icons">
+            <Loader elem={<ResumeIcon />} />
+            <Loader
+              elem={
+                <Icon
+                  name="Games"
+                  link="https://cloudlight-games.itch.io/"
+                  img={itchIcon}
+                />
+              }
+            />
+
+            <Loader
+              elem={
+                <Icon
+                  name="Art"
+                  link="https://www.instagram.com/littleartbean/"
+                  img={instaIcon}
+                />
+              }
+            />
+
+            <Loader
+              elem={
+                <Icon
+                  name="Linkedin"
+                  link="https://www.linkedin.com/in/ben-gillott/"
+                  img={linkedinIcon}
+                />
+              }
+            />
+
+            <Loader
+              elem={
+                <Icon
+                  name="Github"
+                  link="https://github.com/ben-gillott"
+                  img={gitIcon}
+                />
+              }
+            />
           </div>
-        }
-      />
+        </div>
+      </div>
 
       <div className="blurbContainer">
         <Loader elem={<h1>About</h1>} />
 
         <div className="lineContainer">
-          <Loader elem={<div className="hLine "></div>} />
+          <Loader className="hLine" />
         </div>
 
         <Loader
           elem={
             <p className="blurb">
-              I am fascinated by the blend of art and technology used to create
-              digital media. I passionately believe that this combination can
-              and should be used to create profound emotional experiences.
+              I am a software engineer specializing in computer graphics. I am
+              fascinated by the blend of art and technology, and I passionately
+              believe this combination should be used to create profound
+              emotional experiences.
               <br />
               <br />
-              I make independent games under the moniker "Cloudlight Games".
+              In my free time I make indie games, art, and DIY projects.
               <br />
               <br />
-              Graduated from Cornell in 2021 with a B.A. in Computer Science.
-              Currently working at the geospatial startup Whiteout Solutions.
+              I graduated from Cornell in 2021 with a B.A. in Computer Science,
+              and am currently working at the geospatial startup Whiteout
+              Solutions.
               <br />
               <br />
             </p>
           }
         />
-
-        <div className="iconContainer">
-          <Loader elem={<ResumeIcon />} />
-          <Loader
-            elem={
-              <Icon
-                name="Games"
-                link="https://cloudlight-games.itch.io/"
-                img={itchIcon}
-              />
-            }
-          />
-
-          <Loader
-            elem={
-              <Icon
-                name="Art"
-                link="https://www.instagram.com/littleartbean/"
-                img={instaIcon}
-              />
-            }
-          />
-
-          <Loader
-            elem={
-              <Icon
-                name="Linkedin"
-                link="https://www.linkedin.com/in/ben-gillott/"
-                img={linkedinIcon}
-              />
-            }
-          />
-
-          <Loader
-            elem={
-              <Icon
-                name="Github"
-                link="https://github.com/ben-gillott"
-                img={gitIcon}
-              />
-            }
-          />
-        </div>
       </div>
     </div>
   );
