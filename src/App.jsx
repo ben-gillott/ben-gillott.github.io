c717f8efe0a590d550496174cb0e2634db5cfcc7
Ben Gillott <52325353+ben-gillott@users.noreply.github.com>
2023-05-15 17:26:29 -0800

    scroll fix attempts

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,23 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Topbar from "./components/topbar/Topbar";
 import About from "./components/about/About";
 // import Art from "./components/art/Art";
-import { useEffect } from "react";
 
 import "./app.scss";
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  // window.onbeforeunload = function () {
-  //   window.scrollTo(0, 0);
-  // };
-
   const [menuOpen, setMenuOpen] = useState(false);
-
-  useEffect(() => {
-    if (!location.hash) {
-      window.scrollTo(0, 0);
-    }
-  }, [location]);
 
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <ScrollToTop>
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-        </Routes>
-      </ScrollToTop>
+
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+      <Link onClick={window.scrollTo(0, 0)}>Scroll Up</Link>
     </div>
   );
 }
