5bb3c44a62006fee3ec5d640f32527c06717a0a0
Ben Gillott <52325353+ben-gillott@users.noreply.github.com>
2023-08-08 13:25:50 -0700

    Metadata for posts

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { getPosts } from "./PostUtils.jsx";
 
 export default function PostList() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState();
 
   useEffect(() => {
     getPosts().then((d) => setPosts(d));
@@ -14,13 +14,17 @@ export default function PostList() {
     <div className="postlist">
       <h1> PostLists: </h1>
       <ul>
-        {posts.map((p) => (
-          <li key={p.name}>
-            <Link to={`/posts/${p.name}`}>
-              <h3>{p.name}</h3>
-            </Link>
-          </li>
-        ))}
+        {posts !== undefined &&
+          Object.keys(posts).map((slug) => (
+            <li key={slug} style={{ visibility: posts[slug].published ? "visible" : "hidden" }}>
+              <Link to={`/posts/${slug}`}>
+                <div className="postLink">
+                  <h3>{posts[slug].headline}</h3>
+                  <h4>{posts[slug].date} </h4>
+                </div>
+              </Link>
+            </li>
+          ))}
       </ul>
     </div>
   );
--- src/components/posts/PostUtils.jsx
+++ src/components/posts/PostUtils.jsx
@@ -1,18 +1,19 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://api.github.com/repos/ben-gillott/ben-gillott.github.io/contents/src/assets/posts?ref=src",
+  // baseURL: "https://api.github.com/repos/ben-gillott/ben-gillott.github.io/contents/src/assets/posts?ref=src",
+  baseURL: "https://raw.githubusercontent.com/ben-gillott/ben-gillott.github.io/src/src/assets/posts/metadata.json",
 });
 
 export async function getPosts() {
-  const response = await client.get();
-  const retval = response.data.filter((p) => p.type === "file");
-  return retval;
+  const response = (await client.get()).data;
+  console.log(response);
+  return response;
 }
 
 export async function getPostMetadata(slug) {
   const posts = await getPosts();
-  const metadataRet = posts.find((p) => p.name === slug);
+  const metadataRet = Object.keys(posts).at(slug);
   console.log(metadataRet);
   return metadataRet;
 }
